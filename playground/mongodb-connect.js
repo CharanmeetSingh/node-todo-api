1e434f66e1f264c6908465a996a0fd8e17ac1d21
Charanmeet Singh <charanmeet.technicalbee@gmail.com>
2018-08-03 19:25:52 +0530

    Initial commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,46 @@
+const {MongoClient, ObjectID} = require('mongodb');
+//const ObjectID = require('mongodb').ObjectID;
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server.');
+    }
+
+    console.log('MongoDB Server connected successfully.');
+    let db = client.db('TodoApp');
+
+    // db.collection('Todos').insertOne({
+    //     text: 'Something to do',
+    //     completed: false
+    // }, (err, res) => {
+    //     if (err) {
+    //         return console.log('Unable to insert todo', err);
+    //     }
+
+    //     console.log(JSON.stringify(res.ops, undefined, 2));
+    // });
+
+    // db.collection('Users').insertOne({
+    //     name: 'Amanda',
+    //     age: 25,
+    //     location: 'Hollywood'
+    // }, (err, res) => {
+    //     if (err) {
+    //         return console.log('Unable to insert user', err);
+    //     }
+
+    //     console.log(JSON.stringify(res.ops, undefined, 2));
+    // });
+
+    db.collection('Users').find({
+        _id: new ObjectID('5b61a2218b75510cb4f609a4')
+    }).toArray().then((docs) => {
+        console.log('Users');
+        console.log(JSON.stringify(docs, undefined, 2));
+    }, (err) => {
+        console.log('Unable to fetch', err);
+    });
+
+    client.close();
+
+});
