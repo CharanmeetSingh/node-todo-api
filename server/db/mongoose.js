fba583a338361a6292d3e414f3807b60b1c9fda7
Charanmeet Singh <charanmeet.technicalbee@gmail.com>
2018-08-08 19:08:05 +0530

    Delete todos/id added

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 let mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://todo-user:todo-password1@ds061196.mlab.com:61196/node-todo' || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://localhost:27017/TodoApp' || 'mongodb://todo-user:todo-password1@ds061196.mlab.com:61196/node-todo');
 
 module.exports = { mongoose };
